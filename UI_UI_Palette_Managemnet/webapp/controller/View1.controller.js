bf4f5e2b3ccb86a99e9daf6ed1e9e3db88d6fc8c
Vaibhav Singh <vasinghm@in.ibm.com>
2019-05-30 22:34:51 +0530

    commit message

---
+++ UI_UI_Palette_Managemnet/webapp/controller/View1.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/core/mvc/Controller" 
+], function (Controller) {
+ "use strict";
+ return Controller.extend("UI_UI_Palette_Managemnet.UI_UI_Palette_Managemnet.controller.View1", {
+onInit: function () {
+}
+});
+});
